1ceb1f5f919f6abdd169d4669831c112434e7b7c
kchilds1 <kenneth.childs20@gmail.com>
2024-03-31 20:33:01 -0500

    Added click handler for numbers being clicked

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
 import Wrapper from "./components/Wrapper/wrapper";
@@ -6,35 +7,80 @@ import ButtonBox from "./components/ButtonBox/ButtonBox";
 import Button from "./components/Button/Button";
 
 //values to be flatten nested arrays and mapped on the ButtonBox by index
-const btnValues = [
-  ["C", "+-", "%", "/"],
-  [7, 8, 9, "X"],
-  [4, 5, 6, "-"],
-  [1, 2, 3, "+"],
-  [0, ".", "="],
-];
+ const btnValues = [
+   ["C", "+-", "%", "/"],
+   [7, 8, 9, "X"],
+   [4, 5, 6, "-"],
+   [1, 2, 3, "+"],
+   [0, ".", "="],
+ ];
 
 function App() {
+  let [calc, setCalc] = useState({
+    // default setting below
+    sign:"",
+    num: 0,
+    res: 0,
+  })
+
+  const numClickHandler = (e) => {
+    //prevent the default action of clicking an HTML element from happening
+        e.preventDefault();
+      //get the inner html number of the clicked button
+        const value = e.target.innerHTML;
+    //Making sure there are no more that 15 numbers being put in at a time on the screen
+        if (calc.num.length < 16) {
+      
+          setCalc({
+            ...calc,
+            num:
+              calc.num === 0 && value === "0"
+                ? "0"
+          //check to see if we have a whole number for calc.num
+                : calc.num % 1 === 0
+          //If we have a whole number we will convert it to a number
+                ? Number(calc.num + value)
+          //concatenate the numbers
+                : calc.num + value,
+          //If there isn't a math operation set the result to 0, else keep it unchanged 
+            res: !calc.sign ? 0 : calc.res,
+          });
+        }
+      };
+  
   return (
     <>
       <Header />
       <Wrapper>
-        <Screen value="0" />
+        {/* display calc number if there is one or display calc.res on the screen */}
+        <Screen value = {calc.num ? calc.num : calc.res} />
         <ButtonBox>
           {/* flatten nested arrays for btnValues and mapping them by index */}
-          {btnValues.flat().map((btn, i) => {
+           {btnValues.flat().map((btn, i) => { 
             return (
               <Button
                 key={i}
                 // if button is = the css class name is equals.  If not = there isn't a css class assigned
                 className={btn === "=" ? "equals" : ""}
                 value={btn}
-                onClick={() => {
-                  console.log(`${btn} clicked!`);
-                }}
+                onClick={
+                   btn === "C"
+                   ? resetClickHandler
+                   : btn === "+-"
+                   ? invertClickHandler
+                   : btn === "%"
+                   ? percentClickHandler
+                   : btn === "="
+                   ? equalsClickHandler
+                   : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                   ? signClickHandler
+                   : btn === "."
+                   ? commaClickHandler
+                  : numClickHandler
+                }
               />
             );
-          })}
+           })} 
         </ButtonBox>
       </Wrapper>
       <Footer />
